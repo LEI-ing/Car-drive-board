b72dc86b391a291dc35882c770dd1bfe095510d0
LEI <jsksoskjw@gmail.com>
2024-10-26 16:06:55 +0800

    1.新增了滑动均值滤波 2.完善了功能 3.添加了宏，提高了可读性

--- Function/HCSR05.c
+++ Function/HCSR05.c
@@ -4,14 +4,71 @@
 #include "TIM.h"
 #include "Delay.h"
 
+#define WINDOW_SIZE 5
+#define SOUNDSPEED 340
+
+float distance, smooth_distance;
+uint32_t count;
+
+float moving_filter(float new_value)
+{
+    static float window[WINDOW_SIZE] = {0};
+    static uint8_t index = 0;
+    static float sum = 0;
+    static uint8_t count = 0;
+
+    sum -= window[index];
+    window[index] = new_value;
+    index = (index + 1) % WINDOW_SIZE;
+    sum += window[index];
+
+    if(count < WINDOW_SIZE)
+        count++;
+    
+    return sum / count;
+}
+
 void HCSR04_Init(void)
 {
     MyGPIO_Init(HCSR04_PORT, TRIG, GPIO_Mode_Out_PP);
+    GPIO_WriteBit(HCSR04_PORT, TRIG, Bit_RESET);
     MyGPIO_Init(HCSR04_PORT, ECHO, GPIO_Mode_IPD);
     TIM_General_Init(HCSR04_TIM, 20, ENABLE, 1, 1);
+
+    EXTI_InitTypeDef EXTI_InitStruct;
+    EXTI_InitStruct.EXTI_Line = EXTI1_IRQn;
+    EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+    EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+    EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Rising;
+    EXTI_Init(&EXTI_InitStruct);
 }
 
 void TriggerSigner(void)
 {
+    GPIO_WriteBit(HCSR04_PORT, TRIG, Bit_SET);
+    Delay_us(40);
+    GPIO_WriteBit(HCSR04_PORT, TRIG, Bit_RESET);
+    Delay_us(100);
+}
 
+float GetDistance(void)
+{
+    if(count == 0)
+        return -1.0;
+    distance = count / TimerFrequency * SOUNDSPEED / 2;
+    smooth_distance = moving_filter(distance);
+    return smooth_distance;
+}
+
+void EXTI1_IRQHandler(void)
+{
+    if(EXTI_GetFlagStatus(EXTI_Line1) != RESET)
+    {
+        TIM_Cmd(HCSR04_TIM, ENABLE);
+        while(GPIO_ReadInputDataBit(HCSR04_PORT, ECHO) == SET);
+        TIM_Cmd(HCSR04_TIM, DISABLE);
+        count = TIM_GetCounter(HCSR04_TIM);
+        TIM_SetCounter(HCSR04_TIM, 0);
+        EXTI_ClearITPendingBit(EXTI_Line1);
+    }
 }
