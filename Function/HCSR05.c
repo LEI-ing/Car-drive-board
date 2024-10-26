476fef52d04890db241406abb8e058ac6e8997ba
LEI <jsksoskjw@gmail.com>
2024-10-26 14:55:36 +0800

    1.自定义了TIM初始化函数 2.自定义了GPIO初始化函数 3.编写了HCSR04的初始化函数（功能部分进行中）

--- Function/HCSR05.c
+++ Function/HCSR05.c
@@ -1,16 +1,14 @@
 #include "stm32f10x.h" // Device header
 #include "HCSR05.h"
+#include "GPIO.h"
+#include "TIM.h"
 #include "Delay.h"
 
 void HCSR04_Init(void)
 {
-    RCC_APB2PeriphClockCmd(HCSR04_PORT, ENABLE);
-    RCC_APB1PeriphClockCmd(HCSR04_TIM, ENABLE);
-
-    GPIO_InitTypeDef GPIO_InitStruct;
-    GPIO_InitStruct.GPIO_Mode = 
-
-    GPIO_Init(HCSR04_PORT, &GPIO_InitStruct);
+    MyGPIO_Init(HCSR04_PORT, TRIG, GPIO_Mode_Out_PP);
+    MyGPIO_Init(HCSR04_PORT, ECHO, GPIO_Mode_IPD);
+    TIM_General_Init(HCSR04_TIM, 20, ENABLE, 1, 1);
 }
 
 void TriggerSigner(void)
